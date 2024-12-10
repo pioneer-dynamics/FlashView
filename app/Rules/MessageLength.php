f8808e39749d15a4d5473d483a3fb10632f705ea
Mathew Paret <mathewparet@gmail.com>
2024-12-10 22:41:31 +1100

    Fixed and re-organized MessageLength validation rule

--- app/Rules/MessageLength.php
+++ app/Rules/MessageLength.php
@@ -19,20 +19,74 @@ class MessageLength implements ValidationRule
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        $ciphertext = substr($value, 16);
+        $message_length = $this->getActualMessageLength($value);
 
-        $binary = base64_decode($ciphertext);
-
-        $binary_length = strlen($binary);
-
-        $message_length = $binary_length - 28; // 28 is the length of the header
-
-        if ($message_length > $this->length) {
+        if ($message_length > $this->getAllowedMessageLength()) {
             $fail('The :attribute must be at most ' . $this->length . ' characters.');
         }
 
         if($message_length < $this->min_length) {
             $fail('The :attribute must be at least ' . $this->min_length . ' ' . Str::plural('character', $this->min_length) . '.');
         }
+    }
+
+    /**
+     * Get the allowed message length based on the user
+     */
+    private function getAllowedMessageLength(): int
+    {
+        $user = request()->user();
+
+        return match($this->getUserType()) {
+            'subscribed' => $user->plan->settings->messages->message_length,
+            'user' => config('secrets.message_length.user'),
+            'guest' => config('secrets.message_length.guest'),
+        };
+    }
+
+    /**
+     * Identify the type of user submitting the request
+     */
+    private function getUserType(): string
+    {
+        if($user = request()->user()) {
+            if($user->subscribed()) {
+                return 'subscribed';
+            }
+            else {
+                return 'user';
+            }
+        }
+        else {
+            return 'guest';
+        }
+    }
+
+    /**
+     * Get the actual plain-text message length from message
+     */
+    private function getActualMessageLength(string $message): int
+    {
+        /**
+         * Remove the `salt` from the message
+         */
+        $ciphertext = substr($message, 16);
+
+        /**
+         * Decode the message to get the actual binary
+         */
+        $binary = base64_decode($ciphertext);
+
+        /**
+         * Find the binary length
+         */
+        $binary_length = strlen($binary);
+
+        /**
+         * Get the actual message by subtracting the length of the header (28 bytes)
+         */
+        $message_length = $binary_length - 28;
+
+        return $message_length;
     }
 }
