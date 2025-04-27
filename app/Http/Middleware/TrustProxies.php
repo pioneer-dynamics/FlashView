88acc2fbd589e75b91d3c215d662cb3f67d4dda4
Mathew Paret <mathewparet@gmail.com>
2025-04-27 17:46:37 +1000

    More fixing

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -24,6 +24,12 @@ class TrustProxies extends Middleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request);
+        if ($request->headers->has('x-forwarded-proto')) {
+            $header = $request->headers->get('x-forwarded-proto');
+            // Ensure it only has a single value (if there are multiple, take the first one)
+            $request->headers->set('x-forwarded-proto', strtok($header, ','));
+        }
+    
+        return parent::handle($request, $next);
     }
 }
