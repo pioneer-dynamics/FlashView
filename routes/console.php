17ef374bc53dba23faada41f5b0d825578834b80
Mathew Paret <mathewparet@gmail.com>
2025-06-18 20:03:11 +1000

    Remove unwanted jobs

--- routes/console.php
+++ routes/console.php
@@ -2,13 +2,7 @@
 
 use App\Jobs\ClearExpiredSecrets;
 use App\Jobs\PurgeMetadataForExpiredMessages;
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
 
 Schedule::job(new ClearExpiredSecrets)->daily();
 Schedule::job(new PurgeMetadataForExpiredMessages)->daily();
