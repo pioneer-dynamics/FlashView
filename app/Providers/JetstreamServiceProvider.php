a6f4dea5a7ad9f86cb956a594b73bdfbf3a265d9
Mathew Paret <mathewparet@gmail.com>
2026-03-21 16:17:47 +1100

    Add secrets API token abilities to Jetstream permissions
    Register secrets:create, secrets:list, and secrets:delete as valid
    Jetstream API token permissions so they appear in the token
    creation UI and can be assigned to tokens.

--- app/Providers/JetstreamServiceProvider.php
+++ app/Providers/JetstreamServiceProvider.php
@@ -41,6 +41,9 @@ class JetstreamServiceProvider extends ServiceProvider
             'read',
             'update',
             'delete',
+            'secrets:create',
+            'secrets:list',
+            'secrets:delete',
         ]);
     }
 }
