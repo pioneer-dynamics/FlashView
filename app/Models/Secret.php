3e4bf5565ee048cdff0229953eee9960cd244bb9
Mathew Paret <mathewparet@gmail.com>
2026-04-05 16:42:38 +1000

    Encrypt masked_recipient_email at rest and change column to text for PII consistency

--- app/Models/Secret.php
+++ app/Models/Secret.php
@@ -52,6 +52,7 @@ class Secret extends Model
             'message' => 'encrypted',
             'ip_address_sent' => 'encrypted',
             'ip_address_retrieved' => 'encrypted',
+            'masked_recipient_email' => 'encrypted',
         ];
     }
 
