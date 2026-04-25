22587226ab7be1642c88805cd714299c53ab3ff8
Mathew Paret <mathewparet@gmail.com>
2026-04-25 18:20:23 +1000

    Button style

--- resources/js/Pages/Plan/Index.vue
+++ resources/js/Pages/Plan/Index.vue
@@ -124,7 +124,7 @@ const isFreePlan = (plan) => plan.price_per_month == 0
                             <a
                                 v-else
                                 :href="route('plans.subscribe', { plan: plan.id, period: planFrequency })"
-                                class="w-full justify-center"
+                                class="w-full justify-center inline-flex items-center px-4 py-2 bg-gamboge-800 dark:bg-transparent border border-transparent dark:border-gamboge-300 rounded-md font-semibold text-xs text-white dark:text-gamboge-300 uppercase tracking-widest hover:bg-gamboge-700 dark:hover:bg-gamboge-300 dark:hover:text-gray-900 dark:hover:shadow-neon-cyan-sm focus:bg-gamboge-700 dark:focus:bg-gamboge-300 dark:focus:text-gray-900 active:bg-gamboge-900 dark:active:bg-gamboge-300 dark:active:text-gray-900 focus:outline-none focus:ring-2 focus:ring-gamboge-500 focus:ring-offset-2 dark:focus:ring-offset-gray-900 disabled:opacity-50 transition ease-in-out duration-150"
                             >
                                 Choose This Plan
                             </a>
