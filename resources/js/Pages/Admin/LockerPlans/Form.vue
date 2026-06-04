551ecd80624e9ac2bd6ffcd8cc1bad3dbc30b5ff
Mathew Paret <mathewparet@gmail.com>
2026-06-04 13:59:48 +1000

    Locker price input to be whole numbers

--- resources/js/Pages/Admin/LockerPlans/Form.vue
+++ resources/js/Pages/Admin/LockerPlans/Form.vue
@@ -170,10 +170,10 @@ const previewButtonLabel = computed(() => {
                             <TextInput
                                 v-model="amountDollars"
                                 type="number"
-                                step="0.01"
-                                min="0.01"
+                                step="1"
+                                min="0"
                                 class="pl-7 w-full"
-                                placeholder="20.00"
+                                placeholder="20"
                             />
                         </div>
                         <p class="text-xs text-gray-500 dark:text-gray-400 mt-1">
